a8805566baa83f39a6785bf2031a1b090abf26a7
Douglas G. Moore <doug@dglmoore.com>
2018-12-21 06:51:40 +0000

    Test against biological GRNs

--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
 import main
 import neet.boolean as nb
 import neet.synchronous as ns
-import unittest
 import networkx as nx
-import time
 import random
+import time
+import unittest
+import os
 
+from datadex import DataDex
 from itertools import permutations
 from neet.boolean.examples import *
 
@@ -46,6 +48,46 @@ class Attractors(unittest.TestCase):
         got = main.attractors(net)
 
         self.assertUnorderedEqual(expect, got)
+
+    def test_moderate_grns(self):
+        k, N = 0, 0
+        dex = DataDex()
+        data = dex.select(['name', 'filename'], ['is_biological', 'node_count <= 20'])
+        for name, filename in data:
+            exp_filename = os.path.join(filename, 'expressions.txt')
+            ext_filename = os.path.join(filename, 'external.txt')
+            net = nb.LogicNetwork.read_logic(exp_filename, ext_filename)
+            g = net.to_networkx_graph()
+
+            try:
+                start = time.time()
+                got = main.attractors(net)
+                stop = time.time()
+                modular = stop - start
+
+                start = time.time()
+                main.attractors_brute_force(net, encode=True)
+                stop = time.time()
+                brute_force = stop - start
+
+                if brute_force < 1.1*modular:
+                    print("{}: {}".format(name, net.size))
+                    print("  Modules: {}".format(list(map(len, nx.strongly_connected_components(g)))))
+                    print("  DAG: {}".format(list(nx.condensation(g).edges())))
+                    print("  Modular Time:     {}s".format(modular))
+                    print("  Brute Force Time: {}s".format(brute_force))
+                    print("  Factor: {}".format(modular / brute_force))
+
+                k += modular / brute_force
+                N += 1
+
+                expected = ns.attractors(net)
+
+                self.assertUnorderedEqual(expected, got, msg=name)
+            except RuntimeError:
+                pass
+
+        print(k / N)
 
     def assertUnorderedEqual(self, first, second, msg=None):
         self.assertEqual(len(first), len(second), msg)
